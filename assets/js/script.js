9037ffd5e5b669d7ec2b7503dae58179f10a45f7
amiedawn <amie.d.carroll@gmail.com>
2020-10-12 17:38:05 -0600

    storing tasks properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,30 @@ var createTaskHandler = function(event) {
   
   event.preventDefault();
 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+ 
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler); 
   var listItemEl = document.createElement("li"); //create a new task item
   listItemEl.className = "task-item"; //style the new task item
-  listItemEl.textContent = "This is a new task."; //add the text
+  listItemEl.textContent = taskNameInput; //add the text
   tasksToDoEl.appendChild(listItemEl); //append the element to the task list
 
 
